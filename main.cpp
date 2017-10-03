9ca02228798ccb20e70538d38f8fc3a2be19cd6f
maikeal <maikeal@hotmail.com>
2017-10-03 16:51:56 -0400

    Second program

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "sample.h"
+using namespace exercises;
+
+int main()
+{
+  Exercise ex; //Exercise object
+  ex.x = 3; // x fieldf of object
+  ex.y = "Hello"; // y field of object
+  ex.print(); //print() method of object ex
+  std:: cout <<"\n";
+return 0;
+}
